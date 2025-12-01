ea3ca5d7b104d7376986ffae39273409b0211dd8
Thimira Niranjaya <Thimira Niranjaya>
2025-12-01 21:00:09 +0530

    Initial Commit --System Hardware Monitor

---
+++ System Hardware Monitor/HardwareService.cs
@@ -0,0 +1,42 @@
+﻿using LibreHardwareMonitor.Hardware;
+
+public class HardwareService
+{
+    private Computer _computer;
+
+    public HardwareService()
+    {
+        _computer = new Computer
+        {
+            IsCpuEnabled = true,
+            IsGpuEnabled = true, // Crucial for your NVIDIA stats
+            IsMemoryEnabled = true
+        };
+        _computer.Open();
+    }
+
+    public string GetGpuTemperature()
+    {
+        // Iterate through hardware to find the GPU
+        foreach (var hardware in _computer.Hardware)
+        {
+            if (hardware.HardwareType == HardwareType.GpuNvidia)
+            {
+                hardware.Update(); // Refresh the sensor data
+                foreach (var sensor in hardware.Sensors)
+                {
+                    if (sensor.SensorType == SensorType.Temperature)
+                    {
+                        return $"{sensor.Value:F1}°C";
+                    }
+                }
+            }
+        }
+        return "N/A";
+    }
+
+    public void Close()
+    {
+        _computer.Close();
+    }
+}
---
+++ System Hardware Monitor/MainViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Threading;
+
+public class MainViewModel : INotifyPropertyChanged
+{
+    private readonly HardwareService _service;
+    private string _gpuTemp;
+
+    public string GpuTemp
+    {
+        get => _gpuTemp;
+        set { _gpuTemp = value; OnPropertyChanged(); }
+    }
+
+    public MainViewModel()
+    {
+        _service = new HardwareService();
+
+        // Timer to refresh data every 1 second
+        DispatcherTimer timer = new DispatcherTimer();
+        timer.Interval = TimeSpan.FromSeconds(1);
+        timer.Tick += (s, e) => UpdateStats();
+        timer.Start();
+    }
+
+    private void UpdateStats()
+    {
+        GpuTemp = _service.GetGpuTemperature();
+    }
+
+    // Standard MVVM boilerplate
+    public event PropertyChangedEventHandler PropertyChanged;
+    protected void OnPropertyChanged([CallerMemberName] string name = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
+}
